1baacbd2c450e9a74e73b424c3716332ef229138
Michael Williams <michael.williams@enspiral.com>
2018-08-15 23:09:15 -0700

    so many things

--- aburndance.ino
+++ aburndance.ino
@@ -1,3 +1,12 @@
+/*
+
+TODO:
+- change mode or param on button up
+- change brightness while holding prev mode
+- change color correction while holding next mode
+
+*/
+
 #include <Encoder.h>
 #include "FastLED.h"
 #include "LinkedList.h"
@@ -5,11 +14,11 @@
 #include "freertos/task.h"
 #include "math.h"
 
-#define NUM_LEDS (36)
-#define FRAMES_PER_SECOND 60
 
-#define LED_DATA_PIN 13
-#define LED_CLOCK_PIN 12
+#define LED_A_DATA_PIN 13
+#define LED_A_CLOCK_PIN 12
+#define LED_B_DATA_PIN 15
+#define LED_B_CLOCK_PIN 27
 #define LED_CHIPSET APA102
 #define LED_COLOR_ORDER BGR
 
@@ -38,30 +47,70 @@
 
 #define LED_COLOR_TEMPERATURE UncorrectedTemperature
 
-#define MODE_PREV_BUTTON_PIN 26
-#define MODE_NEXT_BUTTON_PIN 25
+#define NUM_BUTTONS 5
 
-#define PARAM_PREV_BUTTON_PIN 34
-#define PARAM_NEXT_BUTTON_PIN 39
+#define MODE_PREV_BUTTON_INDEX 0
+#define MODE_PREV_BUTTON_PIN 25
+#define MODE_NEXT_BUTTON_INDEX 1
+#define MODE_NEXT_BUTTON_PIN 34
 
-#define PARAM_RESET_BUTTON_PIN 36
+#define PARAM_PREV_BUTTON_INDEX 2
+#define PARAM_PREV_BUTTON_PIN 39
+#define PARAM_NEXT_BUTTON_INDEX 3
+#define PARAM_NEXT_BUTTON_PIN 36
+
+#define PARAM_RESET_BUTTON_INDEX 4
+#define PARAM_RESET_BUTTON_PIN 26
 
 #define PARAM_ENCODER_PIN_A 4
 #define PARAM_ENCODER_PIN_B 21
 
-CRGB leds[NUM_LEDS];
+int8_t button_pins[NUM_BUTTONS] = {
+  MODE_PREV_BUTTON_PIN,
+  MODE_NEXT_BUTTON_PIN,
+  PARAM_PREV_BUTTON_PIN,
+  PARAM_NEXT_BUTTON_PIN,
+  PARAM_RESET_BUTTON_PIN
+};
+/*
+button_pins[MODE_PREV_BUTTON_INDEX] = MODE_PREV_BUTTON_PIN;
+button_pins[MODE_NEXT_BUTTON_INDEX] = MODE_NEXT_BUTTON_PIN;
+button_pins[PARAM_PREV_BUTTON_INDEX] = PARAM_PREV_BUTTON_PIN;
+button_pins[PARAM_NEXT_BUTTON_INDEX] = PARAM_NEXT_BUTTON_PIN;
+button_pins[PARAM_RESET_BUTTON_INDEX] = PARAM_RESET_BUTTON_PIN;
+*/
+
+#define BUTTON_HOLD_MILLIS 2000
+bool button_is_pressed_down[NUM_BUTTONS] = { false };
+bool button_is_pressed_up[NUM_BUTTONS] = { false };
+bool button_is_held[NUM_BUTTONS] = { false };
+uint32_t button_start_press_time[NUM_BUTTONS] = { 0 };
+
+
+#define NUM_LEDS_A 36
+#define NUM_LEDS_B 36
+
+#define FRAMES_PER_SECOND 120
+
+CRGB leds_a[NUM_LEDS_A];
+CRGB leds_b[NUM_LEDS_B];
 
 Encoder encoder(PARAM_ENCODER_PIN_A, PARAM_ENCODER_PIN_B);
 
 #define NUM_MODES 3
 int8_t mode_index = 0;
 
+#define DEFAULT_BRIGHTNESS 255
+uint8_t brightness = DEFAULT_BRIGHTNESS;
+
 #define MAX_NUM_PARAMS 3
 int8_t num_params_by_mode[NUM_MODES] = { 2, 2, 1 };
-int16_t params[NUM_MODES][MAX_NUM_PARAMS] = { 0 };
+int16_t params[NUM_MODES][MAX_NUM_PARAMS] = {
+  0, 168, 0,
+  128, 0, 0,
+  64, 0, 0
+};
 int8_t param_index = 0;
-
-uint8_t brightness = 255;
 
 TaskHandle_t ControlTaskHandle, RenderTaskHandle;
 
@@ -109,15 +158,18 @@ void ControlTask (void * parameter) {
     read_param_index();
     read_param_encoder();
 
+    brightness = read_brightness();
+
     delay(20);
   }
 }
 
 void RenderTask (void * parameter) {
-  FastLED.addLeds<LED_CHIPSET, LED_DATA_PIN, LED_CLOCK_PIN, LED_COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_CHIPSET, LED_A_DATA_PIN, LED_A_CLOCK_PIN, LED_COLOR_ORDER>(leds_a, NUM_LEDS_A);
+  FastLED.addLeds<LED_CHIPSET, LED_B_DATA_PIN, LED_B_CLOCK_PIN, LED_COLOR_ORDER>(leds_b, NUM_LEDS_B);
   FastLED.setCorrection(LED_COLOR_CORRECTION);
   FastLED.setTemperature(LED_COLOR_TEMPERATURE);
-  FastLED.setMaxRefreshRate(FRAMES_PER_SECOND);
+  // FastLED.setMaxRefreshRate(FRAMES_PER_SECOND);
   FastLED.countFPS(30);
   FastLED.setMaxPowerInVoltsAndMilliamps(LED_VOLTAGE, LED_MAX_MILLIAMPS);
 
@@ -130,27 +182,58 @@ void RenderTask (void * parameter) {
 
     run_mode();
     FastLED.show();
+    FastLED.delay(1000 / FRAMES_PER_SECOND); 
     fade_all();
   }
 }
 
 /* controls */
 
+
+bool read_button_press (int8_t button_index) {
+  int8_t button_pin = button_pins[button_index];
+  int8_t button_was_pressed_down = button_is_pressed_down[button_index];
+  int8_t button_was_held = button_is_held[button_index];
+  int64_t button_pressed_time = button_start_press_time[button_index];
+  
+  int8_t button_value = digitalRead(button_pin);
+
+  if (button_value == HIGH) {
+    button_is_pressed_down[button_index] = true;
+    button_is_pressed_up[button_index] = false;
+    if (button_pressed_time == 0) {
+      button_start_press_time[button_index] = millis();
+    } else if (millis() - button_pressed_time > BUTTON_HOLD_MILLIS) {
+      button_is_held[button_index] = true;
+    }
+  } else if (button_value == LOW) {
+    button_start_press_time[button_index] = 0;
+    button_is_pressed_down[button_index] = false;
+    button_is_held[button_index] = false;
+    
+    if (button_was_pressed_down && !button_was_held) {
+      button_is_pressed_up[button_index] = true;
+    } else {
+      button_is_pressed_up[button_index] = false;
+    }
+  }
+}
+
 void read_mode_index () {
   bool has_mode_changed = false;
 
-  int8_t prev_mode_sensor = digitalRead(MODE_PREV_BUTTON_PIN);
-  if (prev_mode_sensor == HIGH) {
+  read_button_press(MODE_PREV_BUTTON_INDEX);
+  if (button_is_pressed_up[MODE_PREV_BUTTON_INDEX]) {
     mode_index = mod(mode_index - 1, NUM_MODES);
     has_mode_changed = true;
   }
 
-  int8_t next_mode_sensor = digitalRead(MODE_NEXT_BUTTON_PIN);
-  if (next_mode_sensor == HIGH) {
+  read_button_press(MODE_NEXT_BUTTON_INDEX);
+  if (button_is_pressed_up[MODE_NEXT_BUTTON_INDEX]) {
     mode_index = mod(mode_index + 1, NUM_MODES);
     has_mode_changed = true;
   }
-
+  
   if (has_mode_changed) {
     Serial.print("mode: ");
     Serial.print(mode_index);
@@ -158,8 +241,6 @@ void read_mode_index () {
 
     param_index = 0;
     encoder.write(params[mode_index][param_index]);
-
-    delay(250);
   }
 }
 
@@ -167,15 +248,16 @@ void read_param_index () {
   bool has_param_changed = false;
   int8_t num_params = num_params_by_mode[mode_index];
 
-  int8_t prev_param_sensor = digitalRead(PARAM_PREV_BUTTON_PIN);
-  if (prev_param_sensor == HIGH) {
+  read_button_press(PARAM_PREV_BUTTON_INDEX);
+  if (button_is_pressed_up[PARAM_PREV_BUTTON_INDEX]) {
     param_index = mod(param_index - 1, num_params);
     has_param_changed = true;
   }
 
-  int8_t next_param_sensor = digitalRead(PARAM_NEXT_BUTTON_PIN);
-  if (next_param_sensor == HIGH) {
+  read_button_press(PARAM_NEXT_BUTTON_INDEX);
+  if (button_is_pressed_up[PARAM_NEXT_BUTTON_INDEX]) {
     param_index = mod(param_index + 1, num_params);
+    has_param_changed = true;
   }
 
   if (has_param_changed) {
@@ -184,8 +266,6 @@ void read_param_index () {
     Serial.println();
 
     encoder.write(params[mode_index][param_index]);
-
-    delay(250);
   }
 }
 
@@ -204,16 +284,16 @@ void read_param_encoder () {
     Serial.println();
   }
 
-  int8_t reset_param_sensor = digitalRead(PARAM_RESET_BUTTON_PIN);
-  if (reset_param_sensor == HIGH) {
+
+  read_button_press(PARAM_RESET_BUTTON_INDEX);
+  if (button_is_pressed_up[PARAM_RESET_BUTTON_INDEX]) {
     Serial.println("reset encoders to zero");
     encoder.write(0);
-    delay(250);
   }
 }
 
 uint8_t read_brightness () {
-  uint8_t sensor = 255;
+  uint8_t sensor = DEFAULT_BRIGHTNESS;
   uint8_t brightness = map(sensor, 0, 255, 0, 255);
   Serial.print("brightness: ");
   Serial.print(brightness);
@@ -253,7 +333,8 @@ void rainbow_loop () {
   Serial.print(zoom);
   Serial.println();
 
-  fill_rainbow(leds, NUM_LEDS, floor(rainbow_index), floor(zoom));
+  fill_rainbow(leds_a, NUM_LEDS_A, floor(rainbow_index), floor(zoom));
+  fill_rainbow(leds_b, NUM_LEDS_B, floor(rainbow_index), floor(zoom));
 
   rainbow_index = fmod(rainbow_index + speed, 256.0);
 }
@@ -265,7 +346,7 @@ struct Star {
 LinkedList<Star*> stars = LinkedList<Star*>();
 
 void star_field () {
-  float speed = mapl(params[mode_index][0], 0.0, 255.0, 0.1, 10.0);
+  float speed = mapl(params[mode_index][0], 0.0, 255.0, 0.01, 10.0);
   Serial.print("speed: ");
   Serial.print(speed);
   Serial.println();
@@ -291,12 +372,16 @@ void star_field () {
   while (star_index < stars.size()) {
     struct Star* star = stars.get(star_index);
     int led_index = floor(star->index);
-    if (led_index >= NUM_LEDS) {
+    if (led_index >= (NUM_LEDS_A + NUM_LEDS_B)) {
       stars.remove(star_index);
       free(star);
     } else {
       for (int i = 0; i < speed; i++) {
-        leds[led_index + i] = star->color;
+        if ((led_index + i) < NUM_LEDS_A) {
+          leds_a[NUM_LEDS_A - (led_index + i) - 1] = star->color;
+        } else {
+          leds_b[led_index + i - NUM_LEDS_A] = star->color;
+        }
       }
       star->index += speed;
       star_index++;
@@ -306,10 +391,10 @@ void star_field () {
 
 float convergence_index = 0;
 CRGB convergence_color = random_color();
-uint16_t convergence_midpoint = NUM_LEDS / 2;
+uint16_t convergence_midpoint = NUM_LEDS_A / 2;
 
 void convergence () {
-  float speed = mapl(params[mode_index][0], 0.0, 255.0, 0.1, 10.0);
+  float speed = mapl(params[mode_index][0], 0.0, 255.0, 0.01, 10.0);
   Serial.print("speed: ");
   Serial.print(speed);
   Serial.println();
@@ -321,13 +406,19 @@ void convergence () {
 
   convergence_index += speed;
 
-  fill_solid(leds, convergence_index, convergence_color);
-  fill_solid(leds + (NUM_LEDS - (uint16_t) convergence_index), convergence_index, convergence_color);
+  fill_solid(leds_a, convergence_index, convergence_color);
+  fill_solid(leds_a + (NUM_LEDS_A - (uint16_t) convergence_index), convergence_index, convergence_color);
+
+  fill_solid(leds_b, convergence_index, convergence_color);
+  fill_solid(leds_b + (NUM_LEDS_B - (uint16_t) convergence_index), convergence_index, convergence_color);
 }
 
 void fade_all () {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i].nscale8(250);
+  for (int i = 0; i < NUM_LEDS_A; i++) {
+    leds_a[i].nscale8(250);
+  }
+  for (int i = 0; i < NUM_LEDS_B; i++) {
+    leds_b[i].nscale8(250);
   }
 }
 
